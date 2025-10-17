bb29e0610184090c49ca586581474a5471926522
DyhegoS <dyhegodasilva@gmail.com>
2025-10-17 16:22:28 -0300

    Inicio do desafio

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	
+	@Query("SELECT obj.id, obj.amount, obj.date, obj.seller.name "
+			+ "FROM Sale obj "
+			+ "WHERE obj.date BETWEEN :start AND :end AND UPPER(obj.seller.name) LIKE UPPER('%', :name, '%')")
+	Page<Sale> searchReport(String name, LocalDate start, LocalDate end, Pageable pageable);
 }
